37821eda09ee248f609b7a391b0c1097ef6d016a
sliu369 <2049616299@qq.com>
2025-09-15 17:47:30 -0400

    Complete dialog

--- public/index.js
+++ public/index.js
@@ -1,7 +1,9 @@
 const eventSource = new EventSource('/price/live')
 const priceDisplay = document.getElementById("price-display")
 const outputs = document.querySelector(".outputs")
+const investForm = document.querySelector("form")
 
+//Update gold price every 2 seconds
 eventSource.onmessage = (event) => {
   const data = JSON.parse(event.data)
   const price = data.currentPrice
@@ -13,6 +15,26 @@ eventSource.onerror = () => {
   console.log('Connection failed...')
 }
 
-document.querySelector("form").addEventListener("submit", function(e) {
+//Events when invest button is clicked
+investForm.addEventListener("submit", function(e) {
+  //prevent default behavior and get data from the form
   e.preventDefault()
+  const formData = new FormData(investForm)
+  const invest = formData.get("investment-amount")
+
+  //show dialog
+  outputs.showModal()
+
+  //get the price by the time the button was clicked, convert it to number
+  const currentPrice = Number(priceDisplay.textContent)
+
+  console.log("The data type of the currentPrice is: " + typeof(currentPrice)+ currentPrice)
+  console.log("The data type of the invest is: " + typeof(invest) + invest)
+
+  const message = `You just bought about ${(invest/currentPrice).toFixed(2)} ounces (ozt) for £${invest}. \n You will receive documentation shortly.`
+  document.getElementById("investment-summary").textContent = message
+
+  document.querySelector(".outputs button").addEventListener("click", () => {
+    outputs.close()
+  })
 })
